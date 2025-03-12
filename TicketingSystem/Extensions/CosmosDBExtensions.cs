7117c9ccef5671d3969ae15efd580a904f148107
Zelgiuz <anwebsa91@gmail.com>
2025-03-12 16:13:04 -0400

    Helpful cosmos query extension

---
+++ TicketingSystem/Extensions/CosmosDBExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using System.Linq.Expressions;
+
+public static class CosmosDbExtensions
+{
+    public static async Task<List<T>> QueryAsync<T>(this Container container, Expression<Func<T, bool>> predicate)
+    {
+        var feedIterator = container.GetItemLinqQueryable<T>()
+                                    .Where(predicate)
+                                    .ToFeedIterator();
+
+        List<T> results = new List<T>();
+
+        while (feedIterator.HasMoreResults)
+        {
+            FeedResponse<T> response = await feedIterator.ReadNextAsync();
+            results.AddRange(response);
+        }
+
+        return results;
+    }
+}
