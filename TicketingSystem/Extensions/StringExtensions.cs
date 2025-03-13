e02e10a9d9154abdb01a99f9ace783f791cd2556
Zelgiuz <anwebsa91@gmail.com>
2025-03-13 15:51:47 -0400

    Maybe?

--- TicketingSystem/Extensions/StringExtensions.cs
+++ TicketingSystem/Extensions/StringExtensions.cs
@@ -4,12 +4,12 @@
     {
         public static string ToIso8601(this DateTime dateTime)
         {
-            return dateTime.ToString("yyyy-MM-dd-HH:mm:ssZ");
+            return dateTime.ToString("yyyy-MM-dd-HH:mm:ssK");
         }
 
         public static DateTime FromISO8601(this string dateTime)
         {
-            return DateTime.ParseExact(dateTime, "yyyy-MM-dd-HH:mm:ssZ", null);
+            return DateTime.ParseExact(dateTime, "yyyy-MM-dd-HH:mm:ssK", null);
         }
     }
 }
