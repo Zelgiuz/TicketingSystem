66ce1b7d93aec1b984e4c38ddd669f2e445d8000
Zelgiuz <anwebsa91@gmail.com>
2025-03-13 12:18:21 -0400

    Venue routes

--- TicketingSystem/Controllers/VenuesController.cs
+++ TicketingSystem/Controllers/VenuesController.cs
@@ -13,16 +13,54 @@ namespace TicketingSystem.Controllers
         {
             database = db;
         }
+        //return all venues
         [HttpGet]
-        public async Task<IActionResult> CreateAVenue()
+        public async Task<IActionResult> GetVenues()
         {
             var container = database.GetContainer("Venues");
-            var venue = new Venue()
+            var venues = await container.QueryAsync<Venue>(x => true);
+            return new OkObjectResult(venues);
+        }
+
+        //return available venues date is yyyy-MM-dd format
+        [HttpGet]
+        [Route("Available/{date}")]
+        public async Task<IActionResult> GetAvailableVenues([FromRoute] string date)
+        {
+            var container = database.GetContainer("Venues");
+            var eventsContainer = database.GetContainer("Events");
+
+            var venues = await container.QueryAsync<Venue>(x => true);
+            List<Venue> availableVenues = new List<Venue>();
+            foreach (var venue in venues)
+            {
+                var events = await eventsContainer.QueryAsync<Event>(x => x.VenueId == venue.Id && x.StartDate.StartsWith(date));
+                if (events.Count == 0)
+                {
+                    availableVenues.Add(venue);
+                }
+            }
+            return new OkObjectResult(venues);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> CreateAVenue([FromBody] VenueModel venueModel)
+        {
+            var venue = new Venue
             {
                 Id = Guid.NewGuid().ToString(),
-                Description = "A Test Venue",
-                Name = "Awesome"
+                Name = venueModel.Name,
+                Description = venueModel.Description
             };
+            var container = database.GetContainer("Venues");
+            await container.UpsertItemAsync(venue);
+            return new OkObjectResult(venue);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAVenue([FromBody] Venue venue)
+        {
+            var container = database.GetContainer("Venues");
             await container.UpsertItemAsync(venue);
             return new OkObjectResult(venue);
         }
